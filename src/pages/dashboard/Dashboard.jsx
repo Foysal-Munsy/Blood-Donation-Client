69b4fc142a0c4151f8ccb9ff102ac1deeb40f12d
Foysal-Munsy <bdfoysal555@gmail.com>
2025-07-31 16:47:29 +0600

    volunteer dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import { AuthContext } from "../../providers/AuthProvider";
 import useRole from "../../hooks/useRole";
 import AdminDashboard from "../adminDashboard/AdminDashboard";
 import DonorDashboard from "../donorDashboard/DonorDashboard";
+import VolunteerDashboard from "../volunteerDashboard/VolunteerDashboard";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
@@ -19,19 +20,5 @@ export default function Dashboard() {
     return <DonorDashboard user={user} role={role} />;
   }
 
-  return (
-    <div>
-      {/* Welcome Message */}
-      {user && (
-        <div className="bg-gradient-to-r from-red-600 to-rose-600 text-white py-3 px-4 text-center overflow-hidden">
-          <p className="text-sm md:text-base font-medium animate-pulse">
-            Welcome back, <span className="font-bold">{user.displayName}</span>!
-            <span className="ml-2 hidden sm:inline">Your role is {role}.</span>
-          </p>
-        </div>
-      )}
-
-      <p>G</p>
-    </div>
-  );
+  return <VolunteerDashboard user={user} role={role} />;
 }
